7fa11bfebb58ee940f12f105a96cf37df7203467
hacker6284 <hacker.6284@gmail.com>
2015-12-23 20:04:44 -0700

    Fixed Time Error

--- Sumi-e/Assets/Scripts/PickUp.cs
+++ Sumi-e/Assets/Scripts/PickUp.cs
@@ -5,6 +5,6 @@ public class PickUp : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate (new Vector3 (15, 30, 45) + Time.deltaTime);
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
 	}
 }
