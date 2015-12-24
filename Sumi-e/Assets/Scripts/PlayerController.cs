e3481c06fbb12cfec9901c57ebca986b8b404ce5
hacker6284 <hacker.6284@gmail.com>
2015-12-23 19:32:49 -0700

    Got the Ball Rolling ;)
    It can roll of though so watch out punk.

---
+++ Sumi-e/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate ()
+	{
+		float movehorizontal = Input.GetAxis ("Horizontal");
+		float movevertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (movehorizontal, 0.0f, movevertical);
+
+		rb.AddForce (movement * speed);
+	}
+}
